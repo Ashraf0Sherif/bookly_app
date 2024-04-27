109c1f9412587af7d72836c888d05f01fd6b8742
Ashraf Sherif <shifoashraf75@gmail.com>
2024-04-27 23:53:59 +0300

    create books action

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,40 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      required this.borderRadius,
+      required this.textColor,
+      required this.backgroundColor,
+      required this.text,
+      this.fontSize});
+
+  final BorderRadiusGeometry borderRadius;
+  final Color textColor, backgroundColor;
+  final String text;
+  final double? fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius,
+          ),
+        ),
+        child: Text(
+          text,
+          style: Styles.kTextStyle18.copyWith(
+              color: textColor,
+              fontWeight: FontWeight.w900,
+              fontSize: fontSize),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/core/widgets/custom_button.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
@@ -17,7 +18,7 @@ class BookDetailsView extends StatelessWidget {
             const CustomBookDetailsAppBar(),
             Padding(
               padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.2),
+                  horizontal: MediaQuery.of(context).size.width * 0.24),
               child: const CustomBookItem(),
             ),
             const SizedBox(height: 43),
@@ -37,8 +38,48 @@ class BookDetailsView extends StatelessWidget {
             BookRating(
               mainAxisAlignment: MainAxisAlignment.center,
             ),
+            SizedBox(
+              height: 37,
+            ),
+            BooksAction(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: CustomButton(
+              text: "19.99 €",
+              textColor: Colors.black,
+              backgroundColor: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  bottomLeft: Radius.circular(16)),
+            ),
+          ),
+          Expanded(
+            child: CustomButton(
+              fontSize: 16,
+              text: "Free preview",
+              textColor: Colors.white,
+              backgroundColor: Color(0xffEF8262),
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(16),
+                  bottomRight: Radius.circular(16)),
+            ),
+          ),
+        ],
       ),
     );
   }
