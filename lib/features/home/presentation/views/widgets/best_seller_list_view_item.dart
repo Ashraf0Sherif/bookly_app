1416384ff4493430fea3bcb1efd535cf0f60b5bd
Ashraf Sherif <shifoashraf75@gmail.com>
2024-04-27 21:25:59 +0300

    create custom book details app bar

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../constants.dart';
 import '../../../../../core/utils/assets.dart';
@@ -10,72 +12,77 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 200,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.3 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(11),
-                image: const DecorationImage(
-                  image: AssetImage(
-                    AssetsData.kTestImage,
+    return InkWell(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetails);
+      },
+      child: SizedBox(
+        width: double.infinity,
+        height: 200,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.3 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(11),
+                  image: const DecorationImage(
+                    image: AssetImage(
+                      AssetsData.kTestImage,
+                    ),
+                    fit: BoxFit.fill,
                   ),
-                  fit: BoxFit.fill,
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.43,
-                  child: Text(
-                    "Harry poter and the Goblet of Fire",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style:
-                        Styles.kTextStyle20.copyWith(fontFamily: kGtSectraFine),
-                  ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.43,
-                  child: const Text(
-                    "J.K. Rowling",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.kTextStyle14,
-                  ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "19.99 €",
-                      style: Styles.kTextStyle20
-                          .copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    const Spacer(),
-                    BookRating()
-                  ],
-                ),
-              ],
+            const SizedBox(
+              width: 15,
             ),
-          )
-        ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.43,
+                    child: Text(
+                      "Harry poter and the Goblet of Fire",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.kTextStyle20
+                          .copyWith(fontFamily: kGtSectraFine),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.43,
+                    child: const Text(
+                      "J.K. Rowling",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.kTextStyle14,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "19.99 €",
+                        style: Styles.kTextStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      BookRating()
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
