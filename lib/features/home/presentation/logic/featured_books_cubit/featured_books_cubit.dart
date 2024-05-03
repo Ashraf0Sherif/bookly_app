278978cdb054000cc30e16034553f75452eff534
Ashraf Sherif <shifoashraf75@gmail.com>
2024-05-03 23:50:25 +0300

    create featured books method

--- lib/features/home/presentation/logic/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/logic/featured_books_cubit/featured_books_cubit.dart
@@ -1,10 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../../core/networking/network_exceptions.dart';
 import '../../../data/models/book/book.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  Future<void> getFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var response = await homeRepo.fetchFeaturedBooks();
+    response.when(
+      success: (List<Book> featuredBooks) {
+        emit(FeaturedBooksSuccess(featuredBooks));
+      },
+      failure: (NetworkExceptions networkExceptions) {
+        emit(FeaturedBooksFailure(
+            NetworkExceptions.getErrorMessage(networkExceptions)));
+      },
+    );
+  }
 }
